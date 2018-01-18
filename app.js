f232f0641ff7aae2a70eb7f84d0982dc4db8bbf9
lex alexander <l.alexander10@gmail.com>
2018-01-18 11:17:46 -0800

    Testing new features.

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
-'use strict';
 
 import fs from "fs"
 import path from "path"
 import {promisify} from 'util';
 import moment from 'moment';
+import rmf from "recently-modified-files";
 
+rmf(path.resolve('/Users/lexalexander/Documents/codestuff'), (err, files) => console.log("module ", files))
 const readDir = promisify(fs.readdir)
 const stats = promisify(fs.stat)
-	
+
+const formatTimeStamp = (ts) => moment(ts, moment.ISO_8601).format('MMMM Do YYYY, h:mm:ss a')
+
 const readMe = async () => {
 	try {
 	 let files = await fileInfo()
-	 return files.sort((a,b) => { a.atime - b.atime })
-	 	return files.map((f) => {
-	 		let formattedADate = moment(f['atime']).format('MMMM Do YYYY, h:mm:ss a')
-	 		let formattedMDate = moment(f['mtime']).format('MMMM Do YYYY, h:mm:ss a')
-	 		return Object.assign({}, f, {atime: formattedMDate, mtime: formattedMDate } )
-		})
+	 let retrieveOnlyProperFiles;
+	 
+	 retrieveOnlyProperFiles = files.filter(f => f.stats.isFile())
+	 
+	 return retrieveOnlyProperFiles.sort((a, b) => {
+    	return (a.stats.mtime - b.stats.mtime) ? -1 : ((a.date > b.date) ? 1 : 0) 
+  	}).map((f) => {
+  		return {name: f.name, lastAccessed: formatTimeStamp(f.stats.atime)}
+  	})
 	}
 	catch (e) {
 		return e
 	}
-	
 }
+
 
 const fileInfo = async () => {
 	let fileInfo = []
 	let files = await readDir(path.resolve('.'))
 	let obj = {}
-	return Promise.all( files.map ( async (f) => { 
+	return Promise.all( files.map ( async (f) => {
 		let fStats = await stats( path.resolve('.', f) )
-		return {
-			name: f, 
-			dev: fStats['dev'],
-			mode: fStats['mode'],
-			nlink: fStats['nlink'],
-			uid: fStats['uid'],
-			gid: fStats['gid'],
-			rdev: fStats['rdev'],
-			blksize: fStats['blksize'],
-			ino: fStats['ino'],
-			size: fStats['size'],
-			blocks: fStats['blocks'],
-			atime: fStats['atime'],
-			mtime: fStats['mtime'],
-			ctime: fStats['ctime'],
-			birthtime: fStats['birthtime']
-		}
+			return {name: f, stats: fStats}
 	}))
 }
- 
+
+
+
+let ignoreHiddenFiles = true 
 readMe().then(res => console.log(res))
+
+
+// let times = [ 2017-08-11T18:20:52.000Z,
+//   2017-08-11T18:20:52.000Z,
+//   2017-08-11T05:34:44.000Z,
+//   2017-08-11T18:20:52.000Z,
+//   2017-08-11T05:34:43.000Z,
+//   2017-08-11T18:20:51.000Z ]
+
+//  times.map(t => console.log(formatTimeStamp(t)))
